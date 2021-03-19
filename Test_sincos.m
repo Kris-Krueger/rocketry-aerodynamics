bf88d1ef252438124b1374f6927975790ae344e5
Kris Krueger <76264161+Kris-Krueger@users.noreply.github.com>
2021-03-19 12:28:35 -0400

    Testing Files to Learn MATLAB

---
+++ Test_sincos.m
@@ -0,0 +1,13 @@
+%Comments!!!
+% Function that calculates y and z
+
+%function [exported variable(s)] = function_name(arguments)
+    %function code
+%end
+
+function [y,z] = Test_sincos(bananas)
+   
+y = 2*sin(bananas) - 1;
+z = 1.5*cos(bananas) + 2;
+
+end
