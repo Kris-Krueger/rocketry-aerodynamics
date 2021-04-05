09d69f8654e5f2fb63a92121a417ce2e5e8feb3b
Kris Krueger <76264161+Kris-Krueger@users.noreply.github.com>
2021-04-05 10:20:34 -0400

    Add files via upload

---
+++ ENGR 112 Project/Engr112RocketAssignmentCodeExplained_Filled.m
@@ -0,0 +1,68 @@
+clc
+clear
+%% Instructions for MATLAB modelling
+
+%In MATLAB, as mentioned in the assignment document, you will be
+%computationally calculating certain parameters that will end up allowing
+%you to make design decisions of your rocket fins.
+
+%First, you will need to label all your variables with names and associate
+%your given values for each. An example listing of variables are seen
+%below in Section 1. Write down your variables in Section 2.
+
+%After having all your variables in place, you will have to solve for your
+%unknown variables based off of the equations described in the assignment
+%document.
+
+%The first value you will have to calculate is the Center of Pressure
+%value. After knowning the Center of Pressure, you can manipulate one of
+%the equations enough to find Total Surface Area of the Fins. Lastly, you
+%will have to make a choice on how many fins you want on your rocket. With
+%that value written down, you can find the Cross-Sectional Area of a single
+%fin side with one of the last equations available to you.
+
+%After you have a multitude of parameters (which you just solved for), you
+%can start modelling your fins in SolidWorks.
+
+%% Section 1 [Example Variables]
+%Givens:
+% Cg = (Center of gravity - unknown)
+% SAbody = (Total Body surface area value)
+% SAcone = (Total Cone surface area value)
+% Dfin = (Fin distance from reference point value)
+% Dbody = (Body distance from reference point value) 
+% Dcone = (Cone distance from reference point value) 
+% SM = (Stability margin value) 
+% DiamR = (Rocket Diameter value)
+
+%Unknowns:
+% Cp = (Center of pressure value)
+% Fn = (Fin number - unknown [This will be your choice])
+% SAfin = (Total Fin surface area - unknown)
+% CAfin = (Cross sectional area of a single fin - unknown)
+
+%% Section 2 [Variables]
+
+SAbody =  0.038485 ;
+SAcone =  0.007507 ;
+Dfin =  0.28 ;
+Dbody =  0.17 ;
+Dcone =  0.04 ;
+SM = 2.5 ;
+Fn = 4 ;
+DiamR = 0.0248 ;
+Cg = 0.19 ;
+
+%% Section 3 [Solve for Unknowns]
+format short
+%Solving for Cp
+Cp = Cg - (2*DiamR)
+
+%Solving for SAfin
+SAfin = ((SAbody*Dbody)+(SAcone*Dcone)-(Cp*Dfin)-(Cp*Dcone))/(Cp - Dfin)
+
+%Solving for CAfin 
+CAfin = (SAfin/(Fn*2))
+
+
+
